dd2d5d592c5eee051a54f08e64a5033dae7b45e5
Jon Reinhold <jon.reinhold15@gmail.com>
2019-10-19 20:38:41 -0500

    Create payload.c

---
+++ src/payload.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+   char response[1000] = "Hello World!";
+   FILE *file;
+
+   file = fopen("response.txt", "w");
+   fprintf(file,"%s", response);
+   fclose(file);
+
+   return 0;
+
+}
